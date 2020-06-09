a8613846d0f0995f319f64d8f53cbbc5bac51394
Invinceaman <invinceaman023@gmail.com>
2020-06-09 11:03:30 -0400

    Initial Commit

---
+++ studyguidebank/apps/courses/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Subject, Course, Unit
+# Register your models here.
+
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(Unit)
---
+++ studyguidebank/apps/courses/views.py
@@ -0,0 +1,8 @@
+import random
+
+from django import http
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+
+from .models import Subject, Course, Unit
+# Create your views here.
+
